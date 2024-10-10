b52777bc8d864a5924dbcbc13dc2dbda6597602e
DevBoburHub <boburmamashukurov08@gmail.com>
2024-10-11 01:36:30 +0300

    added

--- Program.cs
+++ Program.cs
@@ -1,13 +1,35 @@
-﻿using System;
+﻿// using System;
 
-Console.Write("Birinchi raqamni kiriting: ");
-double numb1 = Convert.ToDouble(Console.ReadLine());
+// Console.Write("Birinchi raqamni kiriting: ");
+// double numb1 = Convert.ToDouble(Console.ReadLine());
 
-Console.Write("Ikkinchi raqanni kiriting: ");
-double numb2 = Convert.ToDouble(Console.ReadLine());
+// Console.Write("Ikkinchi raqanni kiriting: ");
+// double numb2 = Convert.ToDouble(Console.ReadLine());
 
-double result = numb1 + numb2;
-Console.WriteLine("Natija:  " + result);
+// double result = numb1 + numb2;
+// Console.WriteLine("Natija:  " + result);
+
+// using System;
+
+// Console.Write("N sonini kiriting: ");
+// int N = Convert.ToInt32(Console.ReadLine());
+
+// int sum = N * (N + 1) / 2;
+
+// Console.WriteLine("1 dan " + N + " gacha bo'lgan sonlar yig'indisi: " + sum);
+
+using System;
+
+Console.Write("Raqam kiriting: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+string result = (number % 2 == 0) ? "Juft" : "Toq";
+
+Console.WriteLine(result);
+
+
+
+
 
 
 
