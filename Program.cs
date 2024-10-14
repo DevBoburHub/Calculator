27b898fb4956484c811093e1424162b3fbc1a4da
DevBoburHub <boburmamashukurov08@gmail.com>
2024-10-14 22:05:04 +0300

    Added

--- Program.cs
+++ Program.cs
@@ -1,148 +1,74 @@
 ﻿// using System;
 
-// Console.Write("Birinchi raqamni kiriting: ");
-// double numb1 = Convert.ToDouble(Console.ReadLine());
+// int x = 13;
+// string str = "teLefOn";
 
-// Console.Write("Ikkinchi raqanni kiriting: ");
-// double numb2 = Convert.ToDouble(Console.ReadLine());
+// string result = x > str.Length ? str.ToUpper() : str.ToLower();
 
-// double result = numb1 + numb2;
-// Console.WriteLine("Natija:  " + result);
+// System.Console.WriteLine(result);
 
 // using System;
 
-// Console.Write("N sonini kiriting: ");
-// int N = Convert.ToInt32(Console.ReadLine());
+// int x = 15;
+// int y = 10;
 
-// int sum = N * (N + 1) / 2;
+//  string result;
 
-// Console.WriteLine("1 dan " + N + " gacha bo'lgan sonlar yig'indisi: " + sum);
+//         if (x > y)
+//         {
+//             result = "x is greater than y";
+//         }
+//         else if (x < y)
+//         {
+//             result = "x is less than y";
+//         }
+//         else if (x == y)
+//         {
+//             result = "x is equal to y";
+//         }
+//         else
+//         {
+//             result = "x and y are not comparable";
+//         }
 
-// using System;
-
-// Console.Write("Raqam kiriting: ");
-// int number = Convert.ToInt32(Console.ReadLine());
-
-// string result = (number % 2 == 0) ? "Juft" : "Toq";
-
-// Console.WriteLine(result);
-
-// using  System;
-
-// class Program
-// {
-//     static void Main()
-//     {
-//         Console.Write("Son kiriting: ");
-//         int son = Convert.ToInt32(Console.ReadLine());
-//         int i = 2;
-//         string xabar = "Tub";
-
-//         for (; i * i <= son; i++)
-//             if (son % i == 0)
-//                 xabar = "Tub emas";
-
-//         Console.WriteLine(xabar);
-//     }
-// }
-
+//         Console.WriteLine(result); // Output: x is greater than y
+    
 // using System;
 
 // class Program
 // {
 //     static void Main()
 //     {
-//         Console.Write("Son kiriting: ");
-//         int son = Convert.ToInt32(Console.ReadLine());
-//         string boluvchilar = "";
+//         int x = 15;
+//         int y = 10;
 
-//         for (int i = 2; i <= 10; i++)
-//             if (son % i == 0)
-//                 boluvchilar += i + " ";
+//         string result = (x > y)
+//             ? "x is greater than y"
+//             : (x < y)
+//                 ? "x is less than y"
+//                 : (x == y)
+//                     ? "x is equal to y"
+//                     : "x and y are not comparable";
 
-//         Console.WriteLine("Bo'luvchilari: " + boluvchilar);
-//     }
-// }
-
-
-// using System;
-
-// class Program
-// {
-//     static void Main()
-//     {
-//         Console.Write("Son kiriting: ");
-//         int son = Convert.ToInt32(Console.ReadLine());
-
-//         Console.Write("Daraja kiriting: ");
-//         int daraja = Convert.ToInt32(Console.ReadLine());
-
-//         int natija = 1;
-//         for (int i = 0; i < daraja; i++)
-//             natija *= son;
-
-//         Console.WriteLine("Natija: " + natija);
+//         Console.WriteLine(result); // Output: x is greater than y
 //     }
 // }
 
 // using System;
 
-// class Program
-// {
-//     static void Main()
-//     {
-//         int[] harajatlar = { 150, 230, 80, 120 };
-//         int yigindi = 0;
+//         Console.Write("Hafta kunini kiriting (o'zbek tilida): ");
+//         string haftaKuni = Console.ReadLine();
 
-//         for (int i = 0; i < harajatlar.Length; i++)
-//             yigindi += harajatlar[i];
+//         string englishDay = haftaKuni switch
+//         {
+//             "dushanba" => "Monday",
+//             "seshanba" => "Tuesday",
+//             "chorshanba" => "Wednesday",
+//             "payshanba" => "Thursday",
+//             "juma" => "Friday",
+//             "shanba" => "Saturday",
+//             "yakshanba" => "Sunday",
+//             _ => "Not a valid day"
+//         };
 
-//         Console.WriteLine("Yig'indi: " + yigindi);
-//     }
-// }
-
-// using System;
-
-// class Program
-// {
-//     static void Main()
-//     {
-//         Console.Write("Son kiriting: ");
-//         int son = Convert.ToInt32(Console.ReadLine());
-//         string sonStr = son.ToString();
-//         int uzunlik = sonStr.Length;
-//         int yigindi = 0;
-
-//         for (int i = 0; i < uzunlik; i++)
-//             yigindi += (int)Math.Pow(Convert.ToInt32(sonStr[i].ToString()), uzunlik);
-
-//         Console.WriteLine(son == yigindi ? son + " Armstrong son" : son + " Armstrong son emas");
-//     }
-// }
-
-using System;
-
-class Program
-{
-    static void Main()
-    {
-        Console.Write("Son kiriting: ");
-        int son = Convert.ToInt32(Console.ReadLine());
-        int boluvchilar = 0;
-
-        for (int i = 1; i <= son; i++)
-            if (son % i == 0)
-                boluvchilar++;
-
-        Console.WriteLine("Sifatli bo'luvchilar soni: " + boluvchilar);
-    }
-}
-
-
-
-
-
-
-
-
-
+//         Console.WriteLine($"Ingliz tilida: {englishDay}");
