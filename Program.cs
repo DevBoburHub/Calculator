167e7ecfe390eae63828dbc6833e117dd89a116d
DevBoburHub <boburmamashukurov08@gmail.com>
2024-10-20 00:14:15 +0300

    yangi ozgarish kiritildi

--- Program.cs
+++ Program.cs
@@ -1,74 +1,85 @@
 ﻿// using System;
 
-// int x = 13;
-// string str = "teLefOn";
+// class Program
+// {
+//     static void Main()
+//     {
+//         Console.Write("Daqiqalar sonini kiriting: ");
+//         int daqiqa = Convert.ToInt32(Console.ReadLine()); // Foydalanuvchi daqiqani kiritadi
 
-// string result = x > str.Length ? str.ToUpper() : str.ToLower();
+//         int soat = daqiqa / 60; // Soatlar soni
+//         int qoldiqDaqiqa = daqiqa % 60; // Qolgan daqiqalar
 
-// System.Console.WriteLine(result);
+//         Console.WriteLine("Vaqt: " + soat + ":" + qoldiqDaqiqa); // Soat:daqiqa formatida natija chiqariladi
+//     }
+// }
 
-// using System;
-
-// int x = 15;
-// int y = 10;
-
-//  string result;
-
-//         if (x > y)
-//         {
-//             result = "x is greater than y";
-//         }
-//         else if (x < y)
-//         {
-//             result = "x is less than y";
-//         }
-//         else if (x == y)
-//         {
-//             result = "x is equal to y";
-//         }
-//         else
-//         {
-//             result = "x and y are not comparable";
-//         }
-
-//         Console.WriteLine(result); // Output: x is greater than y
-    
 // using System;
 
 // class Program
 // {
 //     static void Main()
 //     {
-//         int x = 15;
-//         int y = 10;
+//         Console.Write("Yoshingizni kiriting: ");
+//         int yosh = Convert.ToInt32(Console.ReadLine()); // Foydalanuvchidan yoshni olamiz
 
-//         string result = (x > y)
-//             ? "x is greater than y"
-//             : (x < y)
-//                 ? "x is less than y"
-//                 : (x == y)
-//                     ? "x is equal to y"
-//                     : "x and y are not comparable";
+//         string toifa = ""; // Yosh toifasi uchun o'zgaruvchi
 
-//         Console.WriteLine(result); // Output: x is greater than y
+//         if (yosh >= 0 && yosh <= 12) toifa = "Bola";
+//         if (yosh >= 13 && yosh <= 19) toifa = "O'smir";
+//         if (yosh >= 20 && yosh <= 59) toifa = "Katta";
+//         if (yosh >= 60) toifa = "Katta yoshli";
+
+//         Console.WriteLine("Yosh toifasi: " + toifa); // Toifani chiqaramiz
 //     }
 // }
 
 // using System;
 
-//         Console.Write("Hafta kunini kiriting (o'zbek tilida): ");
-//         string haftaKuni = Console.ReadLine();
+// class Program
+// {
+//     static void Main()
+//     {
+//         Console.Write("Birinchi bahoni kiriting: ");
+//         int baho1 = Convert.ToInt32(Console.ReadLine());
 
-//         string englishDay = haftaKuni switch
+//         Console.Write("Ikkinchi bahoni kiriting: ");
+//         int baho2 = Convert.ToInt32(Console.ReadLine());
+
+//         Console.Write("Uchinchi bahoni kiriting: ");
+//         int baho3 = Convert.ToInt32(Console.ReadLine());
+
+//         double ortacha = (baho1 + baho2 + baho3) / 3.0; // Uch bahoning o'rtacha qiymati hisoblanadi
+
+//         string natija = ortacha >= 80 ? "A'lo" : ortacha >= 60 ? "Yaxshi" : ortacha >= 40 ? "Qoniqarli" : "Qoniqarsiz";
+//         Console.WriteLine("Natija: " + natija); // O'rtacha baho asosida natijani chiqaradi
+//     }
+// }
+
+// using System;
+
+// class Program
+// {
+//     static void Main()
+//     {
+//         Random tasodifiy = new Random(); 
+//         int sirliSon = tasodifiy.Next(1, 101); // 1 dan 100 gacha tasodifiy son yaratish
+
+//         Console.WriteLine("1 dan 100 gacha bo'lgan sonni topishga harakat qiling!");
+
+//         bool topildi = false;
+//         int urinish = 0;
+//         while (!topildi)
 //         {
-//             "dushanba" => "Monday",
-//             "seshanba" => "Tuesday",
-//             "chorshanba" => "Wednesday",
-//             "payshanba" => "Thursday",
-//             "juma" => "Friday",
-//             "shanba" => "Saturday",
-//             "yakshanba" => "Sunday",
-//             _ => "Not a valid day"
-//         };
+//             Console.Write("Taxminingizni kiriting: ");
+//             int foydalanuvchiSon = Convert.ToInt32(Console.ReadLine());
+//             urinish++;
 
-//         Console.WriteLine($"Ingliz tilida: {englishDay}");
+//             string natija = foydalanuvchiSon == sirliSon ? "To'g'ri! Siz " + urinish + " urinishda topdingiz." : 
+//                            foydalanuvchiSon > sirliSon ? "Kichikroq son kiriting." : "Kattaroq son kiriting.";
+//             Console.WriteLine(natija);
+
+//             topildi = foydalanuvchiSon == sirliSon; // Agar foydalanuvchi to'g'ri topgan bo'lsa, while loop tugaydi
+//         }
+//     }
+// }
